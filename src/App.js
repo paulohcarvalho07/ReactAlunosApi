056db844adb9e8e9e0f509271f2ba7909740b745
paulohcarvalho07 <169151462+paulohcarvalho07@users.noreply.github.com>
2025-09-17 15:02:53 -0300

    Criando a Aplicação React alunosreact - III

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,30 +7,62 @@ import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
 import logoCadastro from './assets/cadastro.png';
 
 function App() {
-  const baseUrl="https://localhost:44370/api/alunos";
+  const baseUrl = "https://localhost:44370/api/alunos";
 
-  const [data, setData]=useState([]);
+  const [data, setData] = useState([]);
+  const [modalIncluir, setModalIncluir]=useState(false);
 
-  const pedidoget = async()=>{
-    await axios.get(baseUrl)
-    .then(response => {
-      setData(response.data)
-    }).catch(error=>{
-      console.log(error);
-    })
+  const [alunoSelecionado, setAlunoSelecionado]=useState({
+    id: '',
+    nome: '',
+    email: '',
+    idade: ''
+  })
+
+  const abrirFecharModalIncluir=()=>{
+    setModalIncluir(!modalIncluir);
   }
 
-  useEffect(()=>{
-    pedidoget();
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setAlunoSelecionado({
+      ...alunoSelecionado, [name]: value
+    });
+    console.log(alunoSelecionado);
+  }
+
+  const pedidoGet = async () => {
+    await axios.get(baseUrl)
+      .then(response => {
+        setData(response.data)
+      }).catch(error => {
+        console.log(error);
+      })
+  } 
+
+    const pedidoPost = async () => {
+    delete alunoSelecionado.id;
+    alunoSelecionado.idade = parseInt(alunoSelecionado.idade);
+    await axios.post(baseUrl, alunoSelecionado)
+      .then(response => {
+        setData(data.concat(response.data));
+        abrirFecharModalIncluir();
+      }).catch(error => {
+        console.log(error);
+      })
+  }
+
+  useEffect(() => {
+    pedidoGet();
   })
 
   return (
-    <div className="App">
-      <br/>
+    <div className="aluno-container">
+      <br />
       <h3>Cadastro de Alunos</h3>
       <header>
         <img src={logoCadastro} alt='Cadastro' />
-        <button className="btn btn-success">Incluir Novo Aluno</button>
+        <button className="btn btn-success" onClick={() => abrirFecharModalIncluir()}>Incluir Novo Aluno</button>
       </header>
       <table className='table table-bordered'>
         <thead>
@@ -43,22 +75,45 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          <tbody>
-            {data.map(aluno => (
-              <tr key={aluno.id}>
-                <td>{aluno.id}</td>
-                <td>{aluno.nome}</td>
-                <td>{aluno.email}</td>
-                <td>{aluno.idade}</td>
-                <td>
-                  <button className="btn btn-primary">Editar</button> {" "}
-                  <button className="btn btn-danger">Excluir</button>
-                </td>
-              </tr>
-            ))}
-          </tbody>            
+          {data.map(aluno => (
+            <tr key={aluno.id}>
+              <td>{aluno.id}</td>
+              <td>{aluno.nome}</td>
+              <td>{aluno.email}</td>
+              <td>{aluno.idade}</td>
+              <td>
+                <button className="btn btn-primary">Editar</button> {" "}
+                <button className="btn btn-danger">Excluir</button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
+
+      <Modal isOpen={modalIncluir}>
+      <ModalHeader>Incluir Alunos</ModalHeader>
+      <ModalBody>        
+        <div className="form-group">
+          <label>Nome: </label>
+          <br />
+          <input type="text" className="form-control" name="nome" onChange={handleChange}/>
+          <br />
+          <label>Email: </label>
+          <br />
+          <input type="text" className="form-control" name="email" onChange={handleChange}/>
+          <br />
+          <label>Idade: </label>
+          <br />
+          <input type="text" className="form-control" name="idade" onChange={handleChange}/>
+          <br />
+        </div>
+      </ModalBody>
+      <ModalFooter>
+        <button className="btn btn-primary" onClick={() => pedidoPost()}>Incluir</button>{"  "}
+        <button className="btn btn-danger" onClick={() => abrirFecharModalIncluir()}>Cancelar</button>
+      </ModalFooter>
+      </Modal>
+
     </div>
   );
 }
